ac6d75df428d721ca13c038412265f5c3ffeee60
nakamura <nakamura0067@gmail.com>
2021-12-19 00:24:15 +0900

    画面遷移実装

--- Panepone/screen/GameScreen.js
+++ Panepone/screen/GameScreen.js
@@ -2,10 +2,15 @@ import React from 'react';
 import {Text, View, Button} from 'react-native';
 import { commonStyles } from '../styles/commonStyles';
 
-export function GameScreen(){
+export function GameScreen({route}){
+  const {gameSpeed} = route.params;
+  const {switching} = route.params;
   return (
     <View style={commonStyles.container}>
-      <Text style={commonStyles.text}>ゲーム画面</Text>
+      <Text style={commonStyles.text}>ゲーム画面
+        {JSON.stringify(gameSpeed)},
+        {JSON.stringify(switching)}
+      </Text>
     </View>
   );
 }
