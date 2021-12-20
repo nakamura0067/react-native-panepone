239ab728d07710894127525cd52112b2868374b9
nakamura <nakamura0067@gmail.com>
2021-12-20 21:01:16 +0900

    画面要素の追加

--- Panepone/screen/GameScreen.js
+++ Panepone/screen/GameScreen.js
@@ -7,10 +7,28 @@ export function GameScreen({route}){
   const {switching} = route.params;
   return (
     <View style={commonStyles.container}>
-      <Text style={commonStyles.text}>ゲーム画面
-        {JSON.stringify(gameSpeed)},
-        {JSON.stringify(switching)}
-      </Text>
+      <View id="info_area" style={commonStyles.container,{justifyContent:'flex-start'}}>
+        <View id="time_area" style={commonStyles.container,{flexDirection:'row'}}>
+          <Text style={commonStyles.text}>Time</Text>
+          <Text style={commonStyles.text}>00:00</Text>
+        </View>
+        <View id="speed_level_area" style={commonStyles.container,{flexDirection:'row'}}>
+          <Text style={commonStyles.text}>SpeedLv</Text>
+          <Text style={commonStyles.text}>
+            {JSON.stringify(gameSpeed)}
+          </Text>
+        </View>
+        <View id="score_area" style={commonStyles.container,{flexDirection:'row'}}>
+          <Text style={commonStyles.text}>Score</Text>
+          <Text style={commonStyles.text}>0000000000</Text>
+        </View>
+      </View>
+      <View id="game_area" style={commonStyles.container}>
+        <View id="ojama_area" style={commonStyles.container}>
+        </View>
+        <View id="field_area" style={commonStyles.container}>
+        </View>
+      </View>
     </View>
   );
 }
